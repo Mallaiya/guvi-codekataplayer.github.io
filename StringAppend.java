3955fcb0c880682e54a8db8b8c541aa1ff291106
Mallaiya <30726733+Mallaiya@users.noreply.github.com>
2018-01-17 17:49:43 +0530

    Add files via upload

---
+++ StringAppend.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class StringAppend {
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		String append = ".";
+		System.out.println(s.concat(append));
+	}
+}
